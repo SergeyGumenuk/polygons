2d32bb326fd0bc8bd5bfdcb9102191c0a0309884
SergeyGumenuk <audi-1988@inbox.ru>
2023-02-08 01:03:50 +0700

    add mock point

--- polygon/back/views.py
+++ polygon/back/views.py
@@ -4,7 +4,8 @@ from django.http import JsonResponse
 
 def check_point(request):
     if request.method == 'POST':
-        data = json.load(request)   # request.POST
+        data = json.load(request)
         print(data['polygon'])
+        print(data['point'])
         return JsonResponse({'status': 200})
     return JsonResponse({'status': 'error'})
--- polygon/front/static/js/vertices.js
+++ polygon/front/static/js/vertices.js
@@ -21,7 +21,6 @@ function getCookie(name) {
     const cookies = document.cookie.split(";");
     for (let i = 0; i < cookies.length; i++) {
       const cookie = cookies[i].trim();
-      // Does this cookie string begin with the name we want?
       if (cookie.substring(0, name.length + 1) === (name + "=")) {
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
         break;
@@ -39,7 +38,8 @@ $(document).ready(function(){
             url: 'back/ajax/',
             type: 'POST',
             headers: {"X-CSRFToken": getCookie("csrftoken")},
-            data: JSON.stringify({'polygon': polygon,}), //{'name': 'Serg'},
+            data: JSON.stringify({'polygon': polygon,
+                                  'point': [100, 200]}),
             success: function(response){
                 console.log(response)
             }
