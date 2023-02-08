37ce8dd8dc307be633b23b888d88121d8ced4ec1
SergeyGumenuk <audi-1988@inbox.ru>
2023-02-08 23:31:10 +0700

    refactoring

--- polygon/front/static/js/vertices.js
+++ polygon/front/static/js/vertices.js
@@ -63,24 +63,24 @@ function clickToDrawVertex(e){
 
 
 function clickToDrawPoint(e){
-//        console.log(e.offsetX, e.offsetY);
-        canvas_context.beginPath();
-        canvas_context.arc(e.offsetX, e.offsetY, 5, 0, 2 * Math.PI, false);
-        canvas_context.fill();
+//  console.log(e.offsetX, e.offsetY);
+    canvas_context.beginPath();
+    canvas_context.arc(e.offsetX, e.offsetY, 5, 0, 2 * Math.PI, false);
+    canvas_context.fill();
 }
 
 
 function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== "") {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.substring(0, name.length + 1) === (name + "=")) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + "=")) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
     }
-  }
-  return cookieValue;
+    return cookieValue;
 }
