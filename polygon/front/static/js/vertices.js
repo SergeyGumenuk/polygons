4e99c269a2f4cc7fa9529253505c51f98bc3be4f
SergeyGumenuk <audi-1988@inbox.ru>
2023-02-08 21:22:09 +0700

    add draw point functionality

--- polygon/front/static/js/vertices.js
+++ polygon/front/static/js/vertices.js
@@ -1,12 +1,14 @@
 var polygon = [];
 const btn_draw_polygon = document.querySelector('.draw_polygon');
 const btn_set_vertices = document.querySelector('.set_vertices');
+const btn_draw_points_to_check = document.querySelector('.draw_points');
 let canvas = document.getElementById('cvs');
 let canvas_context = canvas.getContext('2d');
 
 
 btn_set_vertices.addEventListener('click', setVertices);
 btn_draw_polygon.addEventListener('click', drawPolygon);
+btn_draw_points_to_check.addEventListener('click', checkPoint);
 
 
 function setVertices() {
@@ -14,54 +16,44 @@ function setVertices() {
 }
 
 
-function clickToDrawVertex(e){
-    clickToDrawPoint(e);
-    //    console.log(e.offsetX, e.offsetY);
-    polygon.push([e.offsetX, e.offsetY]);
-//        canvas_context.beginPath();
-//        canvas_context.arc(e.offsetX, e.offsetY, 5, 0, 2 * Math.PI, false);
-//        canvas_context.fill();
-//        console.log(polygon);
-}
-
-
-function clickToDrawPoint(e){
-//        console.log(e.offsetX, e.offsetY);
-        canvas_context.beginPath();
-        canvas_context.arc(e.offsetX, e.offsetY, 5, 0, 2 * Math.PI, false);
-        canvas_context.fill();
-        console.log(polygon);
-}
-
-
 function drawPolygon() {
     canvas.removeEventListener('click', clickToDrawVertex);
-    for (var i = 0; i < polygon.length; i++) {
-                var point = polygon[i];
+    for (let i = 0; i < polygon.length; i++) {
+                let point = polygon[i];
                 if (i == 0) canvas_context.moveTo(point[0], point[1])
                 else canvas_context.lineTo(point[0], point[1]);
                 canvas_context.stroke();
             }
     canvas_context.fill();
     canvas_context.closePath();
+    console.log(polygon);
 }
 
 
-//[100,200], [200, 100], [400, 350], [100, 300]
-//let c = document.getElementById('cvs');
-//let ctx = c.getContext('2d');
-//
-//
-//for (var i = 0; i < polygon.length; i++) {
-//            var point = polygon[i];
-//            if (i == 0) ctx.moveTo(point[0], point[1])
-//            else ctx.lineTo(point[0], point[1]);
-//            ctx.stroke();
-//        }
-//ctx.fill();
-//ctx.closePath();
-//
-//
+function checkPoint() {
+    canvas_context.fillStyle = "gray"
+    canvas.addEventListener('click', clickToDrawPoint);
+}
+
+
+function clickToDrawVertex(e){
+    clickToDrawPoint(e);
+    polygon.push([e.offsetX, e.offsetY]);
+}
+
+
+function clickToDrawPoint(e){
+        console.log(e.offsetX, e.offsetY);
+        canvas_context.beginPath();
+        canvas_context.arc(e.offsetX, e.offsetY, 5, 0, 2 * Math.PI, false);
+        canvas_context.fill();
+}
+
+
+
+
+
+
 //function getCookie(name) {
 //  let cookieValue = null;
 //  if (document.cookie && document.cookie !== "") {
