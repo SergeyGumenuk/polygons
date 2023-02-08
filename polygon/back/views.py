6e07a597b219b27af1f5a33bea5a1c039b3dc186
SergeyGumenuk <audi-1988@inbox.ru>
2023-02-09 06:10:49 +0700

    working version

--- polygon/back/views.py
+++ polygon/back/views.py
@@ -1,5 +1,5 @@
 import json
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 
 from back.models import Polygon
