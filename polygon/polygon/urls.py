7a688d7958e8fd3e2a447fc3619575487be71534
SergeyGumenuk <audi-1988@inbox.ru>
2023-02-09 23:33:20 +0700

    add regex to the back app path for checkPoint by loading polygons

--- polygon/polygon/urls.py
+++ polygon/polygon/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('back/', include('back.urls', namespace='back')),
+    re_path('.*back/', include('back.urls', namespace='back')),
     path('', include('front.urls', namespace='front'))
 ]
 
